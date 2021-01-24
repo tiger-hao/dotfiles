ee80f1c664eeed335e446153511040a64731eac0
Tiger Hao <tigerhao@live.ca>
2021-01-23 23:09:00 -0800

    add vim gitignore

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,2 +1,4 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~
+cat vim.gitignore >> ~/.gitignore
+git config --global core.excludesFile "~/.gitignore"
