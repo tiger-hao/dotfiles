c67f049c0b5a16169576caa4eaef7af45fcd4ead
Tiger Hao <tiger.hao@shopify.com>
2021-01-29 17:50:52 -0800

    add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -4,7 +4,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 cat vim.gitignore >> ~/.gitignore
 git config --global core.excludesFile "~/.gitignore"
 
-echo 'set -g default-terminal "tmux-256color"' >> ~/.tmux.conf
-echo 'set -ga terminal-overrides ",*256col*:Tc"' >> ~/.tmux.conf
-
 exec $SHELL
