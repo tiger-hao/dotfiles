f8109cacd83f87c53379e24f7a4d2a30db9900e5
Tiger Hao <tigerhao@live.ca>
2021-03-27 19:39:43 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
-cp .vimrc ~
+cp .vimrc .zshrc .tmux.conf ~
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cat vim.gitignore >> ~/.gitignore
-git config --global core.excludesFile "~/.gitignore"
-
-exec $SHELL
+cat .gitconfig >> ~/.gitconfig
