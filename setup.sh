efc64899a2fc605df9417ae1cefe4cea8f5aba3d
Tiger Hao <tigerhao@live.ca>
2021-03-28 00:12:07 -0700

    update coc-settings

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 cp .vimrc .zshrc .tmux.conf ~
+cp coc-settings.json ~/.vim/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cat vim.gitignore >> ~/.gitignore
